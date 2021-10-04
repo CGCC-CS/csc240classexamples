c18e88a786e95dd120945025c5c41fd060bd86df
Wade Huber <wadehuber@gmail.com>
2021-10-04 13:30:20 -0700

    Added more test code, use C++ idioms.

--- assignments/cpp/project3/project3.cpp
+++ assignments/cpp/project3/project3.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<sstream>
 #include<vector>
-#include<stdlib.h>
-#include<string.h>
 #include"term.hpp"
 #include"polynomial.hpp"
 
@@ -10,31 +10,39 @@ using namespace std;
 
 #define BUFFERLEN 100
 
-Term term_from_string(char * buff) {
-    int coef = 0;
-    string var = "";
-    int exp = 0;
-    char * tok = strtok(buff, " ");
-    if(tok != NULL) {
-        coef=atoi(tok);
-	var = std::string(strtok(NULL, " "));
-        exp=atoi(strtok(NULL, " "));
-    }
-    return Term(coef, std::string(var), exp);
+Term term_from_string(string buffer) {
+
+  int coef = 0;
+  string var = "";
+  int exp = 0;
+
+  // Fill the variables from the input buffer
+  istringstream termStream(buffer);
+  termStream >> coef;
+  termStream >> var;
+  termStream >> exp;
+
+  return Term(coef, std::string(var), exp);
 }
 
 void read_objects(std::vector<Term> & term_list) {
 
-    FILE *fp;
-    char buffer[BUFFERLEN];
-    fp = fopen("terms.txt", "r");
+    ifstream termsFile;
+    string buffer;
+    string termFileName = "terms.txt";
 
-    while (fgets(buffer, BUFFERLEN,fp)) {
+    termsFile.open("terms.txt");
+
+    if (!termsFile) {
+      cerr << "Unable to open terms file " << termFileName << "!" << endl;
+    }
+
+    while (getline(termsFile, buffer)) {
       Term this_term;
       this_term =  term_from_string(buffer);
       term_list.push_back(this_term);
     }
-    fclose(fp);
+    termsFile.close();
 }
 
 int main() {
@@ -48,24 +56,35 @@ int main() {
     std::vector<Term> term_list;
 
     cout << "NAME: SAMPLE OUTPUT" << endl << endl;
+
+    // Test Term object
     cout << "Testing term.cpp/hpp:" << endl;
     cout << "testterm1: " << testterm1.toString() << endl;
     cout << "testterm2: " << testterm2.toString() << endl;
     cout << "testterm3: " << testterm3.toString() << endl;
 
-    read_objects(term_list);
-
+    // Test Polynomial object 
+    printf("\nTesting basic polynomial functions: ") ;
+    Polynomial testPoly;
+    cout << "Adding terms ";
+    testPoly.add(testterm1);
+    testPoly.add(testterm2);
+    testPoly.add(testterm3);
+    cout << "& testing print_poly" << endl;
+    testPoly.print();
+    cout << endl;
+    
     // Add polynomials from term_list to polynomial
+    read_objects(term_list);
     for(unsigned int ii=0;ii<term_list.size();ii++) {
       poly1.add(term_list[ii]);
     }
 
-    printf("\nTesting polynomial.cpp/hpp : \n");
-    printf("Original: : ");
+    cout << endl << "Testing polynomial.cpp/hpp : " << endl;
+    cout << "Original: : ";
     poly1.print();
-    cout << endl;
     poly2 = poly1.combineLikeTerms();
-    printf("\nCombined: : ");
+    cout << endl << "Combined: : ";
     poly2.print();
     cout << endl << endl;
 
