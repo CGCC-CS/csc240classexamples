5f9cf770771d18b6ae48101cede5b9116c5c31b2
Wade Huber <whuber@wades-mac-mini.lan>
2021-11-08 21:02:16 -0700

    Fall 2021 example

--- scheme/onlineclass3.scm
+++ scheme/onlineclass3.scm
@@ -28,12 +28,14 @@
 (cons '(1 2 3) '(4 5 6))
 (append '(1 2 3) '(4 5 6))
 
+(newline)
 "Pairs"
 (cons 1 2)
 (define pr '(a . b))
 (car pr)
 (cdr pr)
 (cons 3 '())
+(cons 'm '(n o p))
 
 (newline)
 "Example list procedures"
@@ -50,11 +52,11 @@
     (if (null? lst)
         '()
         (cons (/ (car lst) 2) (halve-each-element (cdr lst))))))
-(halve-each-element '(37 89 44 28))
+(halve-each-element '(37 89 42 28))
 (halve-each-element lst)
 
 (newline)
-"Higher-order functions"
+"Higher-order function"
 (define red
   (lambda (operator base-case lst)
     (if (null? lst)
@@ -65,16 +67,19 @@ lst
 (red + 0 lst)
 "Multiply the list"
 (red * 1 lst)
-"Double every element in a list"
+"Double every element in the list"
 (red (lambda (carval cdrval) (cons (* 2 carval) cdrval)) '() lst)
-"Length"
+"Length of a list"
 (red (lambda (x y) (+ 1 y)) 0 lst)
-(red (lambda (x y) (+ 1 y)) 0 '(1 2 3 4 5 6 7 8 9 10))
+(red (lambda (x y) (+ 1 y)) 0 '(a b c d e f g h i j))
 "Get all even numbers"
-(red (lambda (x y) (if (= 1 (remainder x 2)) y (cons x y))) '() lst)
-"Reverse"
-(red (lambda (x y) (append y (list x))) '() '(a b c d e f g h i))
-(red (lambda (x y) (append y (list x))) '() lst)
+(red (lambda (carval cdrresult) (if (= 1 (remainder carval 2)) cdrresult (cons carval cdrresult))) '() lst)
+"Reverse a list"
+(define rev
+  (lambda (lst)
+    (red (lambda (x y) (append y (list x))) '() lst)))
+(rev lst)
+(rev '(a b c d e f g h i j k l m n o p))
 
 (newline)
 "apply, map, filter"
@@ -83,11 +88,11 @@ lst
 (map square lst)
 (map (lambda (x) (+ x 3)) lst)
 (map (lambda (x) (/ x 2)) lst)
-(map number? '(1 a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
-(filter number? '(1 a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
-(filter number? (list 1 'a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
-
-(map square (filter number? (list 1 'a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4))))
+(define crazylist (list 1 'a '(2 3 4) 5 'six (+ 3 4) "Eight" 9 (/ 40 4)))
+(map number? crazylist)
+(filter number? crazylist)
+(map square (filter number? crazylist))
+(apply + (map square (filter number? crazylist)))
 
 (newline)
 "let structures"
@@ -99,15 +104,11 @@ lst
      (c 12))
   (+ a b c))
 (let
-    ((x 7)
-     (y 2))
-  (+ x y 10))
+    ((a 7)
+     (b (+ a 4))
+     (c (- b 5)))
+  (+ a b c))
 
-; Be carefull!
-(let
-    ((a 5)
-     (b (+ a 1)))  ; bad coding style!!!
-  (+ a b))
 
 (define double-pi-list
   (lambda (lst)
@@ -118,6 +119,7 @@ lst
           '()
           (cons (* pi (double (car lst))) (double-pi-list (cdr lst)))))))
 (double-pi-list lst)
+(double-pi-list (filter number? crazylist))
 
 "let -> lambda"
 (let
@@ -134,9 +136,6 @@ lst
 
 (newline)
 "Currying"
-(define sum
-  (lambda (x y)
-    (+ x y)))
 (define increment-creator
   (lambda (x)
     (lambda (y)
@@ -147,36 +146,24 @@ lst
 (define increment
   (lambda (n)
     ((increment-creator 1) n)))
-(increment 7)
-
 (define add10
   (lambda (n)
     ((increment-creator 10) n)))
+(increment 7)
+(map increment lst)
 (add10 7)
-(map add10 '(7 4 38 89 103))
+(map add10 lst)
 
-; f(x)=ax^2 + bx + c
+; f(x) = ax^2 + bx + c
 (define f
   (lambda (a b c)
     (lambda (x)
       (+ (* a x x) (* b x) c))))
-
-"f(x)=x^2 + x + 1"
+"f(x) = x^2 + x + 1"
 (f 1 1 1)
 ((f 1 1 1) 3)
-"f(x)=2x^2 + 3x - 5"
+"f(x) = 2x^2 + 3x - 5"
 ((f 2 3 -5) 3)
 ((f 2 3 -5) -1)
 ((f 2 3 -5) 10)
 
-(newline)
-"Exercise questions"
-(define parens-depth
-  (lambda (lst)
-    (cond
-      ((not (list? lst)) 0)
-      ((null? lst) 1)
-      (else (max (+ 1 (parens-depth (car lst))) (parens-depth (cdr lst)))))))
-(parens-depth '(1 2 (3 4) (5 (6 (7 8) 9))))
-(parens-depth 'deep)
-(parens-depth '(1 2 3))
