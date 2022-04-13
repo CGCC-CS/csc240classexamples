2c55ab491769e1957f821949a19e191bd4913d03
Wade Huber <wadehuber@gmail.com>
2022-04-13 10:35:11 -0700

    Added mr_robot fact

--- assignments/prolog/practice1.pl
+++ assignments/prolog/practice1.pl
@@ -5,6 +5,7 @@ show(firefly, 2002, 9.1).
 show(the_walking_dead, 2010, 8.4).
 show(game_of_thrones, 2011, 9.4).
 show(silicon_valley, 2014, 8.5).
+show(mr_robot, 2015, 8.6).
 show(atlanta, 2016, 8.5).
 show(for_all_mankind, 2019, 7.9).
 show(shadow_and_bone, 2019, 7.7).
