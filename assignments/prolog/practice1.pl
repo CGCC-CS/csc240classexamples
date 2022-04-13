70eecd8584a9ffad67df7c51aabea5a331c34667
Wade Huber <wadehuber@gmail.com>
2022-04-13 10:33:31 -0700

    Updated movie facts

--- assignments/prolog/practice1.pl
+++ assignments/prolog/practice1.pl
@@ -1,14 +1,12 @@
 show(seinfeld, 1989, 8.9).
-show(twin_peaks, 1990, 8.9).
 show(the_xfiles, 1993, 8.7).
 show(friends, 1994, 8.9).
-show(the_shield, 2002, 8.7).
-show(the_wire, 2002, 9.4).
 show(firefly, 2002, 9.1).
-show(friday_night_lights, 2006, 8.6).
 show(the_walking_dead, 2010, 8.4).
 show(game_of_thrones, 2011, 9.4).
 show(silicon_valley, 2014, 8.5).
-show(mr_robot, 2015, 8.6).
 show(atlanta, 2016, 8.5).
-
+show(for_all_mankind, 2019, 7.9).
+show(shadow_and_bone, 2019, 7.7).
+show(the_mandalorian, 2019, 8.8).
+show(loki, 2021, 8.3).
