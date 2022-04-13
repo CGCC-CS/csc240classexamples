1e5e7d45fb6093f8519617f097c9ef3708619481
Wade Huber <wadehuber@gmail.com>
2022-04-13 11:39:07 -0700

    Added more shows.

--- assignments/prolog/practice1.pl
+++ assignments/prolog/practice1.pl
@@ -3,10 +3,11 @@ show(the_xfiles, 1993, 8.7).
 show(friends, 1994, 8.9).
 show(firefly, 2002, 9.1).
 show(the_walking_dead, 2010, 8.4).
-show(game_of_thrones, 2011, 9.4).
+show(sherlock, 2010, 9.1).
+show(game_of_thrones, 2011, 9.3).
 show(silicon_valley, 2014, 8.5).
-show(mr_robot, 2015, 8.6).
 show(atlanta, 2016, 8.5).
+show(stranger_things, 2016, 8.7).
 show(for_all_mankind, 2019, 7.9).
 show(shadow_and_bone, 2019, 7.7).
 show(the_mandalorian, 2019, 8.8).
