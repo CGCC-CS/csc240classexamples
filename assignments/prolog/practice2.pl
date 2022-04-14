bdab9df51a1c1ed68bde50d6fdc46c7f2581478a
Wade Huber <wadehuber@gmail.com>
2022-04-13 23:16:58 -0700

    More consisten rules.

---
+++ assignments/prolog/practice2.pl
@@ -0,0 +1,42 @@
+parent(ava,bob).
+parent(mike,bob).
+parent(emma, sophia).
+parent(william, sophia).
+parent(emma, mia).
+parent(william, mia).
+parent(bob,alice).
+parent(bob,steve).
+parent(mia,alice).
+parent(mia,steve).
+parent(liam,alexander).
+parent(liam,evelyn).
+parent(liam,isabella).
+parent(sophia,alexander).
+parent(sophia,evelyn).
+parent(sophia,isabella).
+sister(mia, sophia).
+sister(sophia,mia).
+sister(mia, liam).
+sister(alice,steve).
+sister(evelyn, alexander).
+sister(evelyn, isabella).
+sister(isabella, alexander).
+brother(liam, mia).
+brother(liam, sophia).
+brother(steve,alice).
+brother(alexander,evelyn).
+brother(alexander,isabella).
+grandparent(ava,alice).
+grandparent(ava,steve).
+grandparent(mike,alice).
+grandparent(mike,steve).
+grandparent(william,alice).
+grandparent(william,steve).
+grandparent(william,alexander).
+grandparent(william,evelyn).
+grandparent(william,isabella).
+grandparent(emma,alice).
+grandparent(emma,steve).
+grandparent(emma,alexander).
+grandparent(emma,evelyn).
+grandparent(emma,isabella).
