b484a844d91c71976ce96cbf7ec40434551ff6f2
Wade Huber <wadehuber@gmail.com>
2023-02-15 20:44:51 -0700

    Initial example

---
+++ cpp/onlineclass0.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+// C++ comment
+int main() {
+
+    string greeting = "Hello, world!";
+
+    int cout = 4;
+    cout << greeting << endl;
+    return 0;
+
+}
