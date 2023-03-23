fdcc4d0277be21127562227bfbff6a4a91a80254
Wade Huber <wadehuber@gmail.com>
2023-03-22 20:36:57 -0700

    Spring 2023 examples

--- scheme/onlineclass1.scm
+++ scheme/onlineclass1.scm
@@ -1,35 +1,6 @@
 #lang scheme
 
 ; Scheme comments start with a semi-colon
-(+ 1 7)
-(* 8 8)
-(+ 1 2 3)
-(+ 42 9 345 2 34 4 2 8 7 6 5 87)
-(* 10000 10000 437843784343 4432432433 2432432 14 3 484324324233 384324322 40 30)
-
-; Atoms
-1           ; integer
-"Scheme"    ; string
-'Prolog     ; symbol
-227/19      ; fraction
-
-; Defining names
-(define a 10)
-(define class "CSC240")
-(define subject 'Scheme)
-a
-class
-subject
-
-(newline)
-"Predicates"
-(string? subject)
-(string? class)
-(symbol? subject)
-(number? a)
-(number? (+ 3 10))
-(number? class)
-
 
 (newline)
 "Literals"
@@ -39,9 +10,55 @@ subject
 3+2i  ; imaginary literal
 
 (newline)
-"Functions"
+"Simple arithmetic"
+(+ 1 7)
+(* 8 8)
+(+ 1 2 3)
+(+ 42 9 345 2 34 4 2 8 7 6 5 87)
+(* 10000 10000 437843784343 4432432433 2432432 14 3 484324324233 384324322 40 30)
+(/ 20 3)
+(/ 20 3.0)
+
+(newline)
+"Atoms"
+1           ; integer
+3.14159     ; decimal
+"Scheme"    ; string
+'Prolog     ; symbol
+227/19      ; fraction
+
+(newline)
+"Defining names"
+(define a 10)             ; associate the name a to the integer 10
+(define class "CSC240")   ; associate the name class to the string "CSC240"
+(define subject 'Scheme)  ; associate the name subject to the symbol 'Scheme
+a
+class
+subject
+
+(newline)
+"Predicates"
+"number? examples"
+(number? 8)
+(number? a)
+(number? class)
+(number? subject)
+"string? examples"
+(string? "This is a string")
+(string? a)
+(string? class)
+(string? subject)
+"symbol? examples"
+(symbol? 'ThisIsASymbol)
+(symbol? a)
+(symbol? class)
+(symbol? subject)
+
+(newline)
+"Lambdas - unnamed functions"
++
 (lambda (x) (+ x 1))
-((lambda (x) (+ x 1)) 30)
+((lambda (x) (+ x 1)) 10)
 ((lambda (x) (+ x 1)) -42)
 ((lambda (x) (+ x 1)) (+ 7 8))
 ((lambda (x) (+ x 1)) a)
@@ -51,13 +68,15 @@ subject
 "Giving a function a name"
 (define increment (lambda (x) (+ x 1)))
 increment
-(increment 30)
+(increment 10)
 (increment -42)
 (increment (+ 7 8))
 (increment a)
-;(increment)  ; error!
+(increment (* 7 (- (* 4 6) (* 3  2))))
+;(increment)  ; error: arity mismatch!
 
-"min"
+(newline)
+"Defining a min function"
 (define min
   (lambda (a b)
     (if (> b a)
@@ -73,37 +92,64 @@ increment
 '(1 2 3 4)
 (quote (1 2 3 4))
 
+(newline)
 "list vs quote"
-(list a 1 'Hello "World" + (* 2 7))
-(quote (a 1 'Hello "World" + (* 2 7)))
+(list a 1 'hello "World" (* 2 7) / 30)
+'(a 1 'hello "World" (* 2 7) / 30)
+(quote (a 1 'hello "World" (* 2 7) / 30))
+(list a 'b (quote c))  ; (list a b c) gives an error, so b & c must be quoted
+(quote (a b c))
 
-"Defining lists"
+(newline)
+"Define a list (associate a name to a list)"
 (define lst '(a b c d))
-(define lst2 (list a 'b 'c 'd))
+(define lst2 (list a 'b 'c (quote d)))
 lst
 lst2
 
+(newline)
 "List primitives"
-(car lst)
-(cdr lst)
-(cons 0 lst)
+(car lst)      ; first element in the list
+(cdr lst)      ; list with first element removed
+(cons 0 lst)   ; add as first element of the list
 
-(cons lst '(-4 -3 -2 -1 0))
-(append lst '(-4 -3 -2 -1 0))
+(cons lst '(e f g h i))
+(append lst '(e f g h i))
 
-"Accessing elements of a list with car & cdr"
+(newline)
+"Accessing indificual elements of a list with car & cdr"
 (car lst)
 (car (cdr lst))
-(cdr (cdr lst))
 (car (cdr (cdr lst)))
-(cdr (cdr (cdr lst)))
 (car (cdr (cdr (cdr lst))))
+(cdr (cdr (cdr (cdr lst))))
+
+"Shortcuts"
+(car lst)
+(cadr lst)
+(caddr lst)
+(cadddr lst)
+(cddddr lst)
+
+"Accessing members of embedded lists"
+(define emlst '(1 (a b) (2 (3 4) 5) (c d (e f))))
+emlst
+(length emlst)
+(car (cdr emlst))        ; second element in list
+(car (car (cdr emlst)))
+(cdr (car (cdr emlst)))
+(car (cdr (cdr emlst)))  ; third element in list
+(car (car (cdr (cdr emlst))))
+(cdr (car (cdr (cdr emlst))))
+(car (cdr (car (cdr (cdr emlst)))))
 
 (newline)
 "List of procedures"
 (define procs (list sqrt increment list))
 procs
-(car procs)
+(car procs)    ; sqrt
+(cadr procs)   ; increment
+(caddr procs)  ; list
 ((car procs) 25)
-((car (cdr procs)) 25)
-((car (cdr (cdr procs))) 25)
+((cadr procs) 17)
+((caddr procs) 1 2 (/ 6 2) a 'b 'c "Scheme")
