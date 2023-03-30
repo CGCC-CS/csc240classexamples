2f09a9334bc9605b4971fbfbfa2be1a1a5ce1431
Wade Huber <wadehuber@gmail.com>
2023-03-29 20:36:17 -0700

    Spring 2023 class 2 updates

--- scheme/onlineclass2.scm
+++ scheme/onlineclass2.scm
@@ -42,7 +42,7 @@ lst2
 lst6
 
 ; A list procedure that returns a list
-"Add n to everyelement of list"
+"Add n to every element of list"
 (define add-n-to-list
   (lambda (lst n)
     (if (null? lst)
@@ -65,3 +65,95 @@ lst6
 (sum-list (cons 7 lst1))
 (sum-list (add-n-to-list lst1 12))
 (+ (sum-list lst6) (sum-list (add-n-to-list (cons 4 lst1) 6)) (* 5 (length lst2)))
+
+(newline)
+"Remove the odd numbers from a list"
+(define odd?
+  (lambda (x)
+    (not (= (remainder x 2)  0))))
+(odd? -3)
+(odd? 17)
+(odd? 42)
+(define remove-odds
+  (lambda (lst)
+    (cond
+      ((null? lst) '())
+      ((odd? (car lst)) (remove-odds (cdr lst)))
+      (else (cons (car lst) (remove-odds (cdr lst)))))))
+(remove-odds lst1)
+(remove-odds lst6)
+(remove-odds '(1 3 5 7))
+(remove-odds (append lst1 lst6))
+
+(newline)
+"Compare to lists"
+(define list-equal?
+  (lambda (lst1 lst2)
+    (cond
+      ((and (null? lst1) (null? lst2)) #t)
+      ((or (null? lst1) (null? lst2)) #f)
+      ((equal? (car lst1) (car lst2)) (list-equal? (cdr lst1) (cdr lst2)))
+      (else #f))))
+(list-equal? lst3 lst3)
+(list-equal? lst1 '(1 2 3 4))
+(list-equal? lst1 '(4 3 2 1))
+(list-equal? lst1 (cons 1 (cons 2 (cons 3 (cons 4 '())))))
+
+(newline)
+"What is a list?"
+lst1
+(list? lst1)
+(list? (append lst1 (cons 5 (cons 6 lst5))))
+(list? (cons 0 lst2))
+(list? (cons lst2 0))
+(list? (cons 5 lst1))
+(list? (cons lst1 5))
+(list? (cons lst1 (list 5)))
+(list? '())
+(list? (cons 'x '()))
+(list? (cons 'x 'y))
+
+(newline)
+"Pairs - (x . y)"
+(pair? lst1)
+(pair? (append lst1 (cons 5 (cons 6 lst5))))
+(pair? (cons 0 lst2))
+(pair? (cons lst2 0))
+(pair? (cons 5 lst1))
+(pair? (cons lst1 5))
+(pair? (cons lst1 (list 5)))
+(pair? '())
+(pair? (cons 'x '()))
+(pair? (cons 'x 'y))
+
+(newline)
+"list vs pair"
+(car '(1 2))
+(cdr '(1 2))
+(car '(1 . 2))
+(cdr '(1 . 2))
+(car '(a))
+(cdr '(a))
+;(car '())  ; not allowed
+;(cdr '())  ; car & cdr are PAIR operations, () is a list, not a pair
+
+(newline)
+"pair functions"
+(define in-order-pair
+  (lambda (pr)
+    (>= (cdr pr) (car pr))))
+(in-order-pair '(3 . 20))
+(in-order-pair '(4 . 2))
+
+(define raise-to-power
+  (lambda (pr)
+    (expt (car pr) (cdr pr))))
+(raise-to-power '(2 . 5))
+(raise-to-power '(7 . -2))
+        
+(define multiply-by
+  (lambda (pr)
+    (if (null? (car pr))
+        '()
+        (cons (* (car (car pr)) (cdr pr)) (multiply-by (cons (cdr (car pr)) (cdr pr)))))))
+(multiply-by '((7 12 9 3) . 4))
