5ab361a9c1273c8f384f42bbedb68b8994e3dfa7
Wade Huber <wadehuber@gmail.com>
2023-04-17 20:53:34 -0700

    Spring 2023 updates

--- prolog/onlineclass2.pl
+++ prolog/onlineclass2.pl
@@ -1,41 +1,41 @@
-% recursion:
-%   f(0) = 1 
-%   f(n) = 2n + f(n-1)
+% Recursion:
+%   f(0) = 1
+%   f(n) = f(n-1) + 2n
 
-% f(0)=1
+% base case: f(0)
 recfunc(0,1).
-%   f(n)     =     N-1       f(n-1)                   2n    + f(n-1)
-recfunc(N, F) :- N1 is N - 1, recfunc(N1, F1),  F is (2 * N) + F1.
+%  f(n)       =    N - 1           f(n-1)        f(n-1)
+recfunc(N, F) :- N1 is N - 1, recfunc(N1, F1), F is F1 + (2 * N).
 
-% unify two arguments to same value - use the same var name!
-same(X,X).
+% unify two aruments to the same value by using the same variable name
+same(X, X).
 
 % lists & pairs
 % PAIR: [ Head | Tail ]
 % LIST: [] or [ Head | TailIsAList ]
 
 % pairparts/3 - get head & tail of a pair
-pairparts([H|T], H, T).
+pairparts([H | T], H, T).
+
 % sample output
-%    ?- pairparts([1|b],X,Y).
-%    X = 1,
+%    ?- pairparts([a|b], X, Y).
+%    X = a,
 %    Y = b.
 %    
-%    ?- pairparts(X, 2, 3).
-%    X = [2|3].
+%    ?- pairparts(X, 1, 2).
+%    X = [1|2].
 %    
-%    ?- pairparts([1,2,3,4],Car,Cdr).
-%    Car = 1,
-%    Cdr = [2, 3, 4].
+%    ?- pairparts([a,b,c,d,e],Car,Cdr).
+%    Car = a,
+%    Cdr = [b, c, d, e].
 %    
-%    ?- pairparts(Pair, 1, 2).
-%    Pair = [1|2].
+%    ?- pairparts(List, 1, [2,3,4]).
+%    List = [1, 2, 3, 4].
 %    
-%    ?- pairparts(List, a, [b,c,d]).
-%    List = [a, b, c, d].
-%
-%    ?- pairparts(1, X, Y).
+%    ?- pairparts(1,X,Y).
 %    false.
+%    ?- pairparts(X, [1,2,3], [a,b]).
+%    X = [[1, 2, 3], a, b].
 
 % is_pair/1
 is_pair([_|_]).
@@ -46,27 +46,40 @@ is_list([_|T]) :- is_list(T).
 
 % swap_head_tail/2
 swap_head_tail([H|T], [T|H]).
+%sample output
+%   ?- swap_head_tail([a|b],X).
+%   X = [b|a].
+%   
+%   ?- swap_head_tail([a|b],[K|R]).
+%   K = b,
+%   R = a.
+%   
+%   ?- swap_head_tail(7,X).
+%   false.
 
-% at_least_four_elements/1 - does a list have at least 4 elements?
+% at_least_four_element/1 - does a list have at least 4 elements?
 at_least_four_elements([_,_,_,_|_]).
 
-% third_element/2 - what is the 3rd elements in a list?
+% third_element/2 - what is the 3rd element of a list?
 third_element([_,_,Third|_], Third).
 
+% this_is_weird
+this_is_weird(A,B,C,D,E,[A,[B,B],[C|D],[E,E,E]]).
+
 % add_list/2 - add the elements of a list together
-add_list([],0).
+add_list([], 0).
 add_list([H|T], Sum) :- add_list(T, TailSum), Sum is H + TailSum.
 
-% increment/2 - increment each element of a list
-increment([],[]).
+% increment/2 - increment each element is list
+increment([], []).
 increment([H|T], [H1|TailIncremented]) :- H1 is H + 1, increment(T, TailIncremented).
 
-% multiples_of_n/3 - get the multiples of N
+% multiples_of_n/2 - get the multiples of N
 multiples_of_n([], _, []).
-multiples_of_n([H|T], N, [H|TailMultiples]) :- 0 is H mod N, multiples_of_n(T, N, TailMultiples).
-multiples_of_n([H|T], N, TailMultiples) :- X is H mod N, X>0, multiples_of_n(T, N, TailMultiples).
+multiples_of_n([H|T], N, [H | TailMults]) :- 0 is H mod N, multiples_of_n(T, N, TailMults).
+multiples_of_n([H|T], N, TailMults) :- X is H mod N, X > 0, multiples_of_n(T, N, TailMults).
 
 % append/3
 append([], L, L).
 append(L, [], L).
-append([H|T], L, [H|Appended]) :- append(T, L, Appended).
+append([H|T], L, [H|AppendedTL] ) :- append(T, L, AppendedTL).
