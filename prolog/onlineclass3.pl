49268c73c372aeaa8228ece8daef74b191e3717a
Wade Huber <wadehuber@gmail.com>
2023-04-24 20:44:42 -0700

    Spring 2023 example code

--- prolog/onlineclass3.pl
+++ prolog/onlineclass3.pl
@@ -4,22 +4,21 @@
 %   - remove everything else
 
 do_some_stuff([], []) :- !.
-do_some_stuff([H|T], [H2|TResult]) :- number(H), !, H2 is H * H, do_some_stuff(T, TResult).
-do_some_stuff([[_|_] | T], [list|TResult ]) :- !, do_some_stuff(T, TResult).
+do_some_stuff([H|T], [H2 | TResult]) :- number(H), !, H2 is H ** 2, do_some_stuff(T, TResult).
+do_some_stuff([ [_|_] | T], [list | TResult]) :- !, do_some_stuff(T, TResult).
 do_some_stuff([_H | T], TResult) :- do_some_stuff(T, TResult).
 
-% Using assert & retractall
+% using assert & retractall
 :- dynamic favorite_team/1.
 favorite_team(cardinals).
-change_team(NewFavorite) :- retractall(favorite_team(_)), assertz(favorite_team(NewFavorite)).
+change_team(NewFavorite) :- retractall(favorite_team(_)), asserta(favorite_team(NewFavorite)).
 
 % make_change/6 - makes change for any given amount
 make_change(Amount, HalfDollars, Quarters, Dimes, Nickels, Pennies) :-
-	member(HalfDollars, [2,1,0]),
-	member(Quarters, [4,3,2,1,0]),
-	member(Dimes, [10,9,8,7,6,5,4,3,2,1,0]),
 	member(Nickels, [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]),
-	SubTotal is ( (HalfDollars * 50) + (Quarters * 25) + (Dimes * 10) + (Nickels * 5)),
+	member(Dimes, [10,9,8,7,6,5,4,3,2,1,0]),
+	member(Quarters, [4,3,2,1,0]),
+	member(HalfDollars, [2,1,0]),
+	SubTotal is ( (HalfDollars * 50) + (Quarters * 25) + (Dimes * 10) + (Nickels * 5) ), 
 	SubTotal =< Amount,
 	Pennies is Amount - SubTotal.
-
