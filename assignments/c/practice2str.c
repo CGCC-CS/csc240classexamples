d7439ef737d4fd502125e86ce0ad92d063c2e342
Wade Huber <wadehuber@gmail.com>
2024-05-11 16:11:36 -0700

    Updated comments

--- assignments/c/practice2str.c
+++ assignments/c/practice2str.c
@@ -5,15 +5,16 @@ int main(void) {
    int ii, *ptr, arr[3] = {2, 4, 6};
    char *s, str1[] = "java";
  
-   /* 1A */
+   /* Question a */
    printf("1A) str1: size=%lu string=%s\n\n", sizeof(str1), str1);
  
-   /* 1B - print str1 character by character (with ASCII value) */
-   for (ii = 0; ii < 6; ii++)
+   /* Question b - print str1 character by character (with ASCII value) */
+   for (ii = 0; ii < 6; ii++) {
       printf("1B) str1[%d]='%c' (%d)\n", ii, str1[ii], str1[ii]);
+   }
    printf("\n");
  
-   /* 1C */
+   /* Question c */
    s = str1;
    while (*s != '\0') {
       printf("1C) s=%p  *s=%c\n", s, *s);
@@ -21,7 +22,7 @@ int main(void) {
    }
    printf ("\n");
  
-   /* 1D */
+   /* Question d */
    ptr = arr;
    for (ii=0;ii<3;ii++) {
       printf("1D) ptr=%p  *ptr=%d\n", (void *) ptr, *ptr);
