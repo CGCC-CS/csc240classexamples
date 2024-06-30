b41dbd471583cbc4be0ddce548e607a78aad876c
Wade Huber <wadehuber@gmail.com>
2024-06-30 11:33:07 -0700

    Consolidated activity & practice 1

--- assignments/prolog/practice1.pl
+++ assignments/prolog/practice1.pl
@@ -1,3 +1,29 @@
+% state/2 (country, name).
+state(unitedstates, arizona).
+state(unitedstates, california).
+state(unitedstates, newmexico).
+state(unitedstates, texas).
+state(unitedstates, louisiana).
+state(unitedstates, oklahoma).
+state(unitedstates, colorado).
+state(unitedstates, utah).
+state(unitedstates, nevada).
+state(mexico, chihuahua).
+state(mexico, sonora).
+state(mexico, coahuila).
+state(mexico, durango).
+state(mexico, veracruz).
+state(mexico, bajacalifornia).
+
+% stateinfo/3 (Name, Population, Area-sq-mi)
+stateinfo(chihuahua, 3400000, 96000).
+stateinfo(sonora, 2700000, 69000).
+stateinfo(coahuila, 2700000, 58000).
+stateinfo(california, 34000000, 163000).
+stateinfo(texas, 21000000, 269000).
+stateinfo(arizona, 5000000, 111990).
+
+% show/2 (Title, Year, Rating)
 show(seinfeld, 1989, 8.9).
 show(the_xfiles, 1993, 8.7).
 show(friends, 1994, 8.9).
